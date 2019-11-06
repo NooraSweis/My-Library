9e5957ab2e7aebcad0148dbc29cdee705cf39e72
noora <noora.sweis@gmail.com>
2019-11-06 23:31:01 +0200

    added activities and designed main activity

--- app/src/main/java/com/example/dbproject/MainActivity.java
+++ app/src/main/java/com/example/dbproject/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.dbproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
 
+    public void main_to_books(View view) {
+        startActivity(new Intent(this, ShowBooks.class));
+    }
+    public void main_to_readers(View view) {
+        startActivity(new Intent(this, ShowReaders.class));
+    }
+    public void main_to_employees(View view) {
+        startActivity(new Intent(this, ShowEmployees.class));
+    }
+    public void main_to_details(View view) {
+        startActivity(new Intent(this, ShowDetails.class));
     }
 }
